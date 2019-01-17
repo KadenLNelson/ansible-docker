8aee869ad1372b57c13589544bad1528cdc22370
Kaden Nelson <kadenlnelson@gmail.com>
2019-01-16 19:53:20 -0700

    updated ansible-docker

---
+++ README.md
@@ -0,0 +1,2 @@
+# ansible docker
+`ansible-playbook -i ec2.py playbook.yml`
---
+++ cloud-init.aws.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# install python
+sudo apt-get update && \
+sudo apt-get install -y python && \
+rm -rf /var/lib/apt/lists/*
